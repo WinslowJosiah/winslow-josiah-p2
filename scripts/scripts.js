bfaff2e547bd393418bdc4ccc86aa9eb50620528
WinslowJosiah <winslowjosiah@gmail.com>
2019-11-28 06:01:15 -0600

    Added styles; changed functions of { and } commands

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -113,8 +113,629 @@ const randBigInt = function(range) {
 	return result;
 }
 
-// Now the REAL code begins
+// "Other" things
 
-window.onload = function() {
-	// Nothing yet!
+// Gematrimax command definitions
+var gmxCommands = [
+	{
+		val: 1,
+		cmd: "[",
+		desc: "loop while top of stack is nonzero (tested before start)",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			// If value is nonzero...
+			if (val != 0)
+			{
+				// Push onto the address stack
+				this.addressStack.push(this.instructionPointer);
+			}
+			else
+			{
+				// Skip to matching end-while
+				var nestDepth = 0;
+				while (true)
+				{
+					this.instructionPointer++;
+					if (["[", "_["].includes(this.commands[this.instructionPointer].cmd))
+					{
+						nestDepth++;
+					}
+					else if (["]"].includes(this.commands[this.instructionPointer].cmd))
+					{
+						nestDepth--;
+						if (nestDepth < 0) break;
+					}
+				}
+			}
+		}
+	},
+	{
+		val: 2,
+		cmd: "_[",
+		desc: "loop while top of stack is nonzero (tested after end)",
+		func: function(argc, argv) {
+			this.addressStack.push(this.instructionPointer);
+		}
+	},
+	{
+		val: 3,
+		cmd: "]",
+		desc: "end while loop",
+		func: function(argc, argv) {
+			var loopBackPointer = this.addressStack.pop();
+			
+			// If this is a while loop...
+			if ("[" == this.commands[loopBackPointer].cmd)
+			{
+				this.instructionPointer = loopBackPointer - 1;
+			}
+			// If this is a do-while loop...
+			else if ("_[" == this.commands[loopBackPointer].cmd)
+			{
+				var val = this.stack.pop();
+				if (isUndefined(val))
+				{
+					setError("ERROR: Can't pop from empty stack");
+					this.programExecuting = false;
+					return;
+				}
+				if (val != 0)
+				{
+					this.instructionPointer = loopBackPointer - 1;
+				}
+			}
+		}
+	},
+	{
+		val: 4,
+		cmd: "#",
+		desc: "push (number)",
+		func: function(argc, argv) {
+			this.stack.push(BigInt(argv[0]));
+		}
+	},
+	{
+		val: 5,
+		cmd: "=",
+		desc: "push the stack's length",
+		func: function(argc, argv) {
+			this.stack.push(BigInt(this.stack.length));
+		}
+	},
+	{
+		val: 6,
+		cmd: "?",
+		desc: "pop n, then push a random number from 0 to n",
+		func: function(argc, argv) {
+			var randMax = this.stack.pop();
+			if (isUndefined(randMax))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			if (randMax <= 0)
+			{
+				setError("ERROR: Upper bound for random integer must be positive");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(randBigInt(randMax));
+		}
+	},
+	{
+		val: 7,
+		cmd: ";",
+		desc: "pop a value from the stack and discard",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+		}
+	},
+	{
+		val: 8,
+		cmd: "+",
+		desc: "add the top two stack values, then push the result",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(a + b);
+		}
+	},
+	{
+		val: 9,
+		cmd: "-",
+		desc: "subtract the top stack value from the second-from-top stack value, then push the result",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(a - b);
+		}
+	},
+	{
+		val: 10,
+		cmd: "*",
+		desc: "mutiply the top two stack values, then push the result",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(a * b);
+		}
+	},
+	{
+		val: 11,
+		cmd: "/",
+		desc: "divide the second-from-top stack value by the top stack value, then push the result",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			// Special case to handle division by 0 (which is illegal)
+			if (b == 0)
+			{
+				setError("ERROR: Can't divide by zero");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(a / b);
+		}
+	},
+	{
+		val: 12,
+		cmd: "%",
+		desc: "modulo the second-from-top stack value by the top stack value, then push the result",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			// Special case to handle modulo 0 (which is illegal)
+			if (b == 0)
+			{
+				setError("ERROR: Can't modulo by zero");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(a % b);
+		}
+	},
+	{
+		val: 13,
+		cmd: "!",
+		desc: "pop n, then push 1 if n is 0, otherwise push 0",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(BigInt(val == 0 ? 1 : 0));
+		}
+	},
+	{
+		val: 14,
+		cmd: "$",
+		desc: "discard the lower of the two top stack values",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			// Can't use Math.max() because these are BigInts
+			this.stack.push(a > b ? a : b);
+		}
+	},
+	{
+		val: 15,
+		cmd: "&",
+		desc: "duplicate the top stack value",
+		func: function(argc, argv) {
+			var a = this.stack.pop();
+			if (isUndefined(a))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(a);
+			this.stack.push(a);
+		}
+	},
+	{
+		val: 16,
+		cmd: ">",
+		desc: "pop n, then push a copy of the nth stack value from the top",
+		func: function(argc, argv) {
+			var pointer = this.stack.pop();
+			if (isUndefined(pointer))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			pointer = Number(pointer);
+			if (!(pointer >= 0 && pointer < this.stack.length))
+			{
+				setError("ERROR: Stack index out of range");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(this.stack[this.stack.length - 1 - pointer]);
+		}
+	},
+	{
+		val: 17,
+		cmd: "<",
+		desc: "pop n, then push a copy of the nth stack value from the bottom",
+		func: function(argc, argv) {
+			var pointer = this.stack.pop();
+			if (isUndefined(pointer))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			pointer = Number(pointer);
+			if (!(pointer >= 0 && pointer < this.stack.length))
+			{
+				setError("ERROR: Stack index out of range");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(this.stack[pointer]);
+		}
+	},
+	{
+		val: 18,
+		cmd: "\\",
+		desc: "swap the top two stack values",
+		func: function(argc, argv) {
+			var b = this.stack.pop();
+			var a = this.stack.pop();
+			if (isUndefined(a) || isUndefined(b))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			this.stack.push(b);
+			this.stack.push(a);
+		}
+	},
+	{
+		val: 19,
+		cmd: ")",
+		desc: "rotate the entire stack, shifting the top value down to the bottom",
+		func: function(argc, argv) {
+			this.stack = rotateTopToBottom.call(this, this.stack);
+		}
+	},
+	{
+		val: 20,
+		cmd: "(",
+		desc: "rotate the entire stack, shifting the bottom value up to the top",
+		func: function(argc, argv) {
+			this.stack = rotateBottomToTop.call(this, this.stack);
+		}
+	},
+	{
+		val: 21,
+		cmd: "}",
+		desc: "pop n, then rotate the top n stack values, shifting the top value down to the bottom",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			if (val == 0)
+			{
+				setError("Can't rotate 0 values");
+				this.programExecuting = false;
+				return;
+			}
+			else if (val < 0)
+			{
+				setError("Can't rotate a negative number of values");
+				this.programExecuting = false;
+				return;
+			}
+			else
+			{
+				// This is to ensure that we only rotate as many values as are on the stack
+				if (val > this.stack.length) val = BigInt(this.stack.length);
+				var rotated = [];
+				for (var i = 0; i < val; i++)
+				{
+					var a = this.stack.pop();
+					if (isUndefined(a))
+					{
+						setError("ERROR: Can't pop from empty stack");
+						this.programExecuting = false;
+						return;
+					}
+					rotated.unshift(a);
+				}
+				rotated = rotateTopToBottom.call(this, rotated);
+				this.stack = this.stack.concat(rotated);
+			}
+		}
+	},
+	{
+		val: 22,
+		cmd: "{",
+		desc: "pop n, then rotate the top n stack values, shifting the bottom value up to the top",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			if (val == 0)
+			{
+				setError("Can't rotate 0 values");
+				this.programExecuting = false;
+				return;
+			}
+			else if (val < 0)
+			{
+				setError("Can't rotate a negative number of values");
+				this.programExecuting = false;
+				return;
+			}
+			else
+			{
+				// This is to ensure that we only rotate as many values as are on the stack
+				if (val > this.stack.length) val = BigInt(this.stack.length);
+				var rotated = [];
+				for (var i = 0; i < val; i++)
+				{
+					var a = this.stack.pop();
+					if (isUndefined(a))
+					{
+						setError("ERROR: Can't pop from empty stack");
+						this.programExecuting = false;
+						return;
+					}
+					rotated.unshift(a);
+				}
+				rotated = rotateBottomToTop.call(this, rotated);
+				this.stack = this.stack.concat(rotated);
+			}
+		}
+	},
+	{
+		val: 23,
+		cmd: ",",
+		desc: "take one Unicode codepoint as input and push it onto the stack",
+		func: function(argc, argv) {
+			if (this.eofReached)
+			{
+				this.stack.push(BigInt(-1));
+			}
+			else
+			{
+				this.stack.push(BigInt(this.inputCodepoints[this.inputPointer].codePointAt(0)));
+				this.inputPointer++;
+				if (this.inputPointer >= this.inputCodepoints.length) this.eofReached = true;
+			}
+		}
+	},
+	{
+		val: 24,
+		cmd: ".",
+		desc: "output the top stack value as a Unicode codepoint and discard it",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			// If the number is not within valid Unicode codepoint range...
+			if (!(val >= 0 && val < 1114112))
+			{
+				setError("ERROR: Unicode codepoint out of range");
+				this.programExecuting = false;
+				return;
+			}
+			addToOutput(String.fromCodePoint(Number(val)));
+		}
+	},
+	{
+		val: 25,
+		cmd: ":",
+		desc: "output the top stack value as a number and discard it",
+		func: function(argc, argv) {
+			var val = this.stack.pop();
+			if (isUndefined(val))
+			{
+				setError("ERROR: Can't pop from empty stack");
+				this.programExecuting = false;
+				return;
+			}
+			addToOutput(val.toString());
+		}
+	},
+	{
+		val: 26,
+		cmd: "@",
+		desc: "end the program here",
+		func: function(argc, argv) {
+			this.programExecuting = false;
+		}
+	}
+];
+
+// Helper functions for Gematrimax commands
+function rotateTopToBottom(arr) {
+	var a = arr.pop();
+	if (isUndefined(a))
+	{
+		setError("ERROR: Can't rotate empty stack");
+		this.programExecuting = false;
+		return;
+	}
+	arr.unshift(a);
+	
+	return arr;
+}
+
+function rotateBottomToTop(arr) {
+	var a = arr.shift();
+	if (isUndefined(a))
+	{
+		setError("ERROR: Can't rotate empty stack");
+		this.programExecuting = false;
+		return;
+	}
+	arr.push(a);
+	
+	return arr;
+}
+
+// Convert a word to a gematria value
+function gematria(word) {
+	// Conversion table
+	var letters = {
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+		"e": 5,
+		"f": 6,
+		"g": 7,
+		"h": 8,
+		"i": 9,
+		"j": 10,
+		"k": 11,
+		"l": 12,
+		"m": 13,
+		"n": 14,
+		"o": 15,
+		"p": 16,
+		"q": 17,
+		"r": 18,
+		"s": 19,
+		"t": 20,
+		"u": 21,
+		"v": 22,
+		"w": 23,
+		"x": 24,
+		"y": 25,
+		"z": 26
+	};
+	
+	// Remove accents and diacritics
+	word = word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	// Turn word lowercase
+	word = word.toLowerCase();
+	// Sum up the values of the letters
+	sum = 0n;
+	for (var i = 0; i < word.length; i++)
+	{
+		if (letters[word[i]]) sum += BigInt(letters[word[i]]);
+	}
+	return sum;
+}
+
+// Encode TIO state to URL
+function TIOStateToURL(tioState) {
+	return byteStringToBase64(byteArrayToByteString(pako.deflateRaw(byteStringToByteArray(tioState.join("\xff")), {"level": 9})));
+}
+
+// Decode TIO state from URL
+function URLToTIOState(url) {
+	// Decode it
+	tioState = byteArrayToByteString(pako.inflateRaw(base64ToByteString(url)));
+	// Split it on \xFF characters
+	tioState = tioState.split("\xff");
+	return tioState;
+}
+
+// These functions are stolen from TIO.run
+function textToByteString(string) {
+	return unescape(encodeURIComponent(string));
+}
+
+function byteStringToText(byteString) {
+	return decodeURIComponent(escape(byteString));
+}
+
+function byteStringToBase64(byteString) {
+	return btoa(byteString).replace(/\+/g, "@").replace(/=+/, "");
+}
+
+function base64ToByteString(base64String) {
+	return atob(unescape(base64String).replace(/@|-/g, "+").replace(/_/g, "/"));
+}
+
+function byteArrayToByteString(byteArray) {
+	var retval = "";
+	iterate(byteArray, function(byte) { retval += String.fromCharCode(byte); });
+	return retval;
+}
+
+function byteStringToByteArray(byteString) {
+	var byteArray = new Uint8Array(byteString.length);
+	for(var index = 0; index < byteString.length; index++)
+		byteArray[index] = byteString.charCodeAt(index);
+	byteArray.head = 0;
+	return byteArray;
+}
+
+function iterate(iterable, monad) {
+	if (!iterable)
+		return;
+	for (var i = 0; i < iterable.length; i++)
+		monad(iterable[i]);
 }
